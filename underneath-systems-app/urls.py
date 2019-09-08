19107e530dabf5103fc4db7f48a03a03983efd7c
@JOMS <passwd@ciencias.unam.mx>
2019-09-08 18:20:03 -0500

    Epic weekend refactorization

--- underneath-systems-app/urls.py
+++ underneath-systems-app/urls.py
@@ -1,4 +1,4 @@
-"""PersonalBlog URL Configuration
+"""underneath-systems-app URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
