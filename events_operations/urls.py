db895fd1090ffb0fe89290718a58185edaf8e99e
@JOMS <passwd@ciencias.unam.mx>
2019-09-13 17:20:14 -0500

    Html views for Organizer role

--- events_operations/urls.py
+++ events_operations/urls.py
@@ -6,4 +6,10 @@ urlpatterns = [
     # path('', views.index, name='index'),
     path('', views.mainEvents.as_view(), name='mainEvents'),
     path('create', views.Create.as_view(), name='create'),
+    path('details', views.EventDetails.as_view(), name='details'),
+    path('update', views.CancelEvent.as_view(), name='update'),
+    path('cancel', views.CancelEvent.as_view(), name='cancel'),
+    path('tag', views.TagEvent.as_view(), name='tag'),
+    path('staff', views.StaffEvent.as_view(), name='staff'),
+    path('cancelInvitation', views.CancelInvitation.as_view(), name='cancelInvitation'),
 ]
