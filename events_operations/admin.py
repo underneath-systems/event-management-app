8df2d97d627cf95061178f675794e15e71c08c2e
@JOMS <passwd@ciencias.unam.mx>
2019-09-20 23:21:28 -0500

    Update admin for category table deletion

--- events_operations/admin.py
+++ events_operations/admin.py
@@ -4,10 +4,10 @@ from django.contrib import admin
 # from .models import Organizers, Event, Staff, Category, Tag
 # from events_operations.models import Organizer, Event
 # , Staff, Category, Tag
-from events_operations.models import Organizer, Event, Staff, Category, Tag
+from events_operations.models import Organizer, Event, Staff, Tag
 
 admin.site.register(Organizer)
 admin.site.register(Event)
 admin.site.register(Staff)
-admin.site.register(Category)
+# admin.site.register(Category)
 admin.site.register(Tag)
