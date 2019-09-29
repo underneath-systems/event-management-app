1cad7cf34c48f40ac1d8b5b0d6ee7a2a51d46a68
@JOMS <passwd@ciencias.unam.mx>
2019-09-29 00:57:55 -0500

    Added login required decorator!

--- users_operations/urls.py
+++ users_operations/urls.py
@@ -2,14 +2,15 @@ from django.urls import path
 from django.conf.urls import include, url
 from . import views
 from events_operations.views import *
+from django.contrib.auth.decorators import login_required
 
 app_name = 'users_operations'
 
 urlpatterns = [
-    path('', views.mainUsers.as_view(), name='mainUsers'),
-    path('register_attendee', views.register_Attendee.as_view(), name='register_attendee'),
-    path('register_organizer', views.register_Organizer.as_view(), name='register_organizer'),
-    path('register_staff', views.register_Staff.as_view(), name='register_staff'),
+    path('', login_required(views.mainUsers.as_view()), name='mainUsers'),
+    path('register_attendee', login_required(views.register_Attendee.as_view()), name='register_attendee'),
+    path('register_organizer', login_required(views.register_Organizer.as_view()), name='register_organizer'),
+    path('register_staff', login_required(views.register_Staff.as_view()), name='register_staff'),
     # url(r'^update/(?P<event_id>\d+)/$', event_update, name='event_update'),
     # url(r'^delete/(?P<event_id>\d+)/$', event_delete, name='event_delete'),
 ]
