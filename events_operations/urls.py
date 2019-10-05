d6c34aa7661ffc4d734a6825ba7aff2fdd3783eb
@JOMS <passwd@ciencias.unam.mx>
2019-10-05 05:22:43 -0500

    Sending email for event_create() working!

--- events_operations/urls.py
+++ events_operations/urls.py
@@ -9,7 +9,8 @@ app_name = 'events_operations'
 urlpatterns = [
     # path('nuevo', PostresListado.as_view(template_name = "postres/index.html"), name='leer'),
     path('', login_required(views.mainEvents.as_view()), name='mainEvents'),
-    path('create', login_required(views.Create.as_view()), name='create'),
+    # path('create', login_required(views.Create.as_view()), name='create'),
+    path('create', login_required(event_create), name='create'),
     url(r'^update/(?P<event_id>\d+)/$', login_required(event_update), name='event_update'),
     url(r'^delete/(?P<event_id>\d+)/$', login_required(event_delete), name='event_delete'),
     path('details/', login_required(views.eventsList.as_view(template_name = "event/details.html")), name='details'), 
