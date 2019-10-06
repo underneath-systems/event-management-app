5ea2399eca8d35f22f5628d9ebbed2b62b4de79d
@JOMS <passwd@ciencias.unam.mx>
2019-10-05 22:49:38 -0500

    Sending email for email_update(), email_delete() working

--- events_operations/views.py
+++ events_operations/views.py
@@ -91,59 +91,101 @@ def event_create(request):
 
 
 def event_update(request, event_id):
-	event = Event.objects.get(pk=event_id)
-	if request.method == 'GET':
-		form = eventForm(instance=event)
-	else:
-		form = eventForm(request.POST, instance=event)
-		if form.is_valid():
-			form.save()
-		return redirect('events_operations:details')
-	return render(request, 'event/event_form.html', {'form':form})
+        event = Event.objects.get(pk=event_id)
+        if request.method == 'GET':
+                form = eventForm(instance=event)
+        else:
+                form = eventForm(request.POST, instance=event)
+                if form.is_valid():
+                        form.save()
+                        name = request.POST.get('name')
+                        description = request.POST.get('description')
+                        organizer = request.POST.get('organizer')
+                        start = request.POST.get('start_date_time')
+                        end = request.POST.get('end_date_time')
+                        address = request.POST.get('address')
+                        attendees = request.POST.getlist('attendees_list')
+                        staff = request.POST.get('staff_list')
+                        tag = request.POST.get('tag')
+                        capacity = request.POST.get('capacity')
+
+                        organizer_body = render_to_string(
+                        'event/email_event_update_organizer.html', {
+                                'Nombre': name,
+                                'Descripcion': description,
+                                'Organizador': organizer,
+                                'FechaInicio': start,
+                                'FechaFin': end,
+                                'Direccion': address,
+                                'InvitadosIniciales': attendees,
+                                'Staff': staff,
+                                'Etiquetas': tag,
+                                'Capacidadmaxima': capacity
+                                },
+                        )
+
+
+                        attendees_body = render_to_string(
+                        'event/email_event_update_attendees.html', {
+                                'Nombre': name,
+                                'Descripcion': description,
+                                'Organizador': organizer,
+                                'FechaInicio': start,
+                                'FechaFin': end,
+                                'Direccion': address,
+                                'InvitadosIniciales': attendees,
+                                'Staff': staff,
+                                'Etiquetas': tag,
+                                'Capacidadmaxima': capacity
+                                },
+                        )
+
+                        organizer_email_message = EmailMessage(
+                                subject= 'Has actualizado el evento: ' + name + ' exitosamente. | Underneath Systems',
+                                body=organizer_body,
+                                from_email='sorgv.47@gmail.com',
+                                to=['sorgv.47@gmail.com'],
+                        )
+
+                        attendees_email_message = EmailMessage(
+                                subject= 'El evento: '+name+ ' fue actualizado. | Underneath Systems',
+                                body=organizer_body,
+                                from_email='sorgv.47@gmail.com',
+                                to=['sorgv.47@gmail.com'],
+                        )
+
+                        organizer_email_message.content_subtype = 'html'
+                        attendees_email_message.content_subtype = 'html'
+                        organizer_email_message.send()
+                        attendees_email_message.send()
+                        return redirect('events_operations:details')
+        return render(request, 'event/event_form.html', {'form':form})
 
 
 def event_delete(request, event_id):
-	event = Event.objects.get(pk=event_id)
-	if request.method == 'POST':
-		event.delete()
-		return redirect('events_operations:details')
-	return render(request, 'event/event_delete.html', {'event':event})
+        event = Event.objects.get(pk=event_id)
+        if request.method == 'POST':
+                name = event.name
+                attendees = request.POST.getlist('attendees_list')
+                deleted_event_body = render_to_string(
+                'event/email_event_deleted.html', {
+                        'Nombre': name,
+                        },
+                )
 
-def search(request):
-    query = request.GET.get('q', '')
-    if query:
-        qset = (
-            Q(title__icontains=query) |
-            Q(authors__first_name__icontains=query) |
-            Q(authors__last_name__icontains=query)
-        )
-        results = Book.objects.filter(qset).distinct()
-    else:
-        results = []
-    return render_to_response("books/search.html", {
-        "results": results,
-        "query": query
-    })
+                deleted_email_message = EmailMessage(
+                        subject= 'El evento: '+str(name)+ ' fue cancelado. | Underneath Systems',
+                        body=deleted_event_body,
+                        from_email='sorgv.47@gmail.com',
+                        to=['sorgv.47@gmail.com'],
+                )
 
-def write_organizer(request, idEvent):
-    p = get_object_or_404(Question, pk=idEvent)
-    try:
-        selected_choice = p.choice_set.get(pk=request.POST['choice'])
-    except (KeyError, Choice.DoesNotExist):
-        # Redisplay the question voting form.
-        return render(request, 'polls/detail.html', {
-            'question': p,
-            'error_message': "You didn't select a choice.",
-        })
-    else:
-        selected_choice.votes += 1
-        selected_choice.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
-        return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
-
-
+                deleted_email_message.content_subtype = 'html'
+                event.delete()
+                deleted_email_message.send()
+                print("----- Evento ", name, " eliminado-----")
+                return redirect('events_operations:details')
+        return render(request, 'event/event_delete.html', {'event':event})
 
 
 class eventsList(ListView):
@@ -240,18 +282,6 @@ class TagEvent(SuccessMessageMixin, DeleteView):
         messages.success (self.request, (success_message))       
         return reverse('events') # Redireccionamos a la vista principal 'leer'
 
-
-
-class StaffEvent(SuccessMessageMixin, DeleteView): 
-    model = Event
-    form = Event
-    fields = "__all__"     
-
-    # Redireccionamos a la página principal luego de eliminar un registro o postre
-    def get_success_url(self): 
-        success_message = 'Staff asignado Correctamente !' # Mostramos este Mensaje luego de Editar un Postre 
-        messages.success (self.request, (success_message))       
-        return reverse('events') # Redireccionamos a la vista principal 'leer'
 
 
 
