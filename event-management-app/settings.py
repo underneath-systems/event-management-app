e4338677d024c1919a70ae716a64bbdc9c27f60d
@JOMS <passwd@ciencias.unam.mx>
2019-10-06 18:07:03 -0500

    Update settings

--- event-management-app/settings.py
+++ event-management-app/settings.py
@@ -143,5 +143,5 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 25
 EMAIL_HOST_USER = 'underneath.systems@gmail.com'
-EMAIL_HOST_PASSWORD = 'underneath.33'
+EMAIL_HOST_PASSWORD = 'Underneath.33'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
--- users_operations/views.py
+++ users_operations/views.py
@@ -11,6 +11,8 @@ from django.contrib import messages
 from django.template import loader
 from django.views import generic
 from django import forms
+from django.template.loader import render_to_string
+from django.core.mail import EmailMessage
 
 class mainUsers(View):
     template = 'users/index.html'
@@ -22,6 +24,49 @@ class mainUsers(View):
         """
         print("Main users page request")
         return render(request, self.template, self.context)
+
+def register_attendee(request):
+        if request.method == 'POST':
+                form = attendeesForm(request.POST)
+                if form.is_valid():
+                        form.save()
+                        name = request.POST.get('name')
+                        email = request.POST.get('email')
+                        password = request.POST.get('password')
+                        photo_id = request.POST.get('photo_id')
+                        qr_code = request.POST.get('qr_code')
+
+                        attendees_body = render_to_string(
+                        'event/email_event_creation_organizer.html', {
+                                'Nombre': name,
+                                'Email': email,
+                                'Password': password,
+                                'Fotografia': photo_id,
+                                'Codigo QR': qr_code,
+                                },
+                        )
+
+
+                        attendees_email_message = EmailMessage(
+                                subject= 'Te han invitado registrado como asistente!:  | Underneath Systems',
+                                body=organizer_body,
+                                from_email='sorgv.47@gmail.com',
+                                to=['sorgv.47@gmail.com'],
+                        )
+
+                        organizer_email_message.content_subtype = 'html'
+                        attendees_email_message.send()
+                        print("------Enviando notificaciones por email------")
+                return redirect('events_operations:details')
+        else:
+                form = eventForm()
+        return render(request, 'users/attendees_form.html', {'form':form})
+
+
+
+
+
+
 
 class register_Attendee(CreateView):
     model = Attendees
